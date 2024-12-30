ca0b6eb379d10ae0a8e0dade67c42b9f0cc8a76b
SaiAdithya3 <saiadithyakancharla@gmail.com>
2024-12-30 10:35:49 +0530

    added assets and colors

---
+++ theme/colors.ts
@@ -0,0 +1,11 @@
+// main colourway
+
+export const colors = {
+    primary: '#7BDCBA',
+    secondary: '#9BC8FF',
+    altOnePrimary: '#8DCOFF',
+    altOneSecondary: '#FFBBD3',
+    altTwoPrimary: '#FCB9C2',
+    altTwoSecondary: '#FDD79D',
+    fontColor: '#554b6a'
+}
